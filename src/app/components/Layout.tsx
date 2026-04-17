420bb9ec668958c83fbf9b40ef3f772cb3366b42
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:31:07 +0800

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -50,3 +50,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     <Icon size={16} />
                     {label}
                   </Link>
+                );
