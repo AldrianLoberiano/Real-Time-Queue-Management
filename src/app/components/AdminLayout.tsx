78e6010a9afd37e2fd04e4661be9a53e649cc6d1
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:13:24 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -147,3 +147,4 @@ export function AdminLayout({ children }: { children: React.ReactNode }) {
 
       {/* Main content */}
       <div className="flex-1 flex flex-col min-w-0">
+        {/* Top bar */}
