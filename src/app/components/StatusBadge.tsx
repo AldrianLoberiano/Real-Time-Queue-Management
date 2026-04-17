141a78598ebd33026ed436fce59e1f459fe1d44a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:49:58 +0800

    Update StatusBadge.tsx

--- src/app/components/StatusBadge.tsx
+++ src/app/components/StatusBadge.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { Clock, PlayCircle, CheckCircle, SkipForward } from 'lucide-react';
 import type { StatusType } from '../context/QueueContext';
 
+interface StatusBadgeProps {
