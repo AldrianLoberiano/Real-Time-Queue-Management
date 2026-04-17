c04aab32e47ebe64e9bc5cea2c9bab2478c7df15
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 22:30:19 +0800

    Update avatar.tsx

--- src/app/components/ui/avatar.tsx
+++ src/app/components/ui/avatar.tsx
@@ -42,3 +42,4 @@ function AvatarFallback({
     <AvatarPrimitive.Fallback
       data-slot="avatar-fallback"
       className={cn(
+        "bg-muted flex size-full items-center justify-center rounded-full",
