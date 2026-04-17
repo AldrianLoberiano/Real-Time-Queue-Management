feaecd186f197982b03bb9c7b0152c2f7ee908f5
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:32:38 +0800

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -79,3 +79,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     </span>
                   )}
                 </button>
+                <AnimatePresence>
