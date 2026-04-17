49cc23faa8feddd653829a3fb17a99c544771add
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 22:17:38 +0800

    Update alert.tsx

--- src/app/components/ui/alert.tsx
+++ src/app/components/ui/alert.tsx
@@ -41,3 +41,4 @@ function AlertTitle({ className, ...props }: React.ComponentProps<"div">) {
       className={cn(
         "col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight",
         className,
+      )}
