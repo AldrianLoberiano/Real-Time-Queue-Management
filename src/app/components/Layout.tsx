b80c966fc9aaa1f473c9ef224e620c432e0ef09f
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:36:55 +0800

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -127,3 +127,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 {menuOpen ? <X size={20} /> : <Menu size={20} />}
               </button>
             </div>
+          </div>
