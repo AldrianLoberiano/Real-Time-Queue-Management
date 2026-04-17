0c749a2f328de96d1aa3bf436def7853378aeed4
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:13:22 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -146,3 +146,4 @@ export function AdminLayout({ children }: { children: React.ReactNode }) {
       </AnimatePresence>
 
       {/* Main content */}
+      <div className="flex-1 flex flex-col min-w-0">
