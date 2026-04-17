96f3aeedbb98bba8ef604fd3c26c4a3439c0d0fc
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:59:34 +0800

    Update accordion.tsx

--- src/app/components/ui/accordion.tsx
+++ src/app/components/ui/accordion.tsx
@@ -58,3 +58,4 @@ function AccordionContent({
       className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
       {...props}
     >
+      <div className={cn("pt-0 pb-4", className)}>{children}</div>
