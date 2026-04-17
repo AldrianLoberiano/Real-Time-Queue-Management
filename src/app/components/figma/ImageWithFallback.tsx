d60f5315f379e1ce352b27e324d8963b7bc8b63b
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:25:02 +0800

    Update ImageWithFallback.tsx

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -17,3 +17,4 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
       className={`inline-block bg-gray-100 text-center align-middle ${className ?? ''}`}
       style={style}
     >
+      <div className="flex items-center justify-center w-full h-full">
