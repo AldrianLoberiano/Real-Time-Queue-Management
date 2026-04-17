d7d48445cfca0709b40267e8efa5469ddda2e8ae
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 22:14:26 +0800

    Update alert.tsx

--- src/app/components/ui/alert.tsx
+++ src/app/components/ui/alert.tsx
@@ -11,3 +11,4 @@ const alertVariants = cva(
         default: "bg-card text-card-foreground",
         destructive:
           "text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90",
+      },
