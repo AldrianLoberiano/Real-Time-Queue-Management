e8c6a8aeeff05c9526f08e20b56366c6615c3a2a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:07:50 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -80,3 +80,4 @@ export function AdminLayout({ children }: { children: React.ReactNode }) {
         <div className="pt-2 border-t border-white/10">
           <Link
             to="/"
+            onClick={() => setSidebarOpen(false)}
