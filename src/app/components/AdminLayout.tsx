38c8569a6295ffb4cbbf8d0cd5f010c584b66665
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:11:30 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -126,3 +126,4 @@ export function AdminLayout({ children }: { children: React.ReactNode }) {
           <>
             <motion.div
               initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
