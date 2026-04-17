8ba696c8075ca48b7391b04022c11af182ec985d
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:03:31 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -38,3 +38,4 @@ export function AdminLayout({ children }: { children: React.ReactNode }) {
             <p className="text-white font-bold leading-none">QueueSmart</p>
             <p className="text-blue-400 text-xs">Admin Panel</p>
           </div>
+        </div>
