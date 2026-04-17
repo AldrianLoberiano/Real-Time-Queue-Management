d7522f037c6748f1ea99eea65d1708e00960fed2
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:29:13 +0800

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -27,3 +27,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
               <div className="w-9 h-9 rounded-xl flex items-center justify-center" style={{ background: 'linear-gradient(135deg, #0ea5e9, #38bdf8)' }}>
                 <Users size={20} className="text-white" />
               </div>
+              <div>
