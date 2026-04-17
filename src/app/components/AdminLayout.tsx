b855300564a1a3395a17b694a3fa3637be144d3e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:06:28 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -69,3 +69,4 @@ export function AdminLayout({ children }: { children: React.ReactNode }) {
                   ? 'bg-sky-500 text-white shadow-md'
                   : 'text-blue-200 hover:bg-white/10 hover:text-white'
               }`}
+            >
