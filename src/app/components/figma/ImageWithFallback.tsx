19c85bef993dc25fcad802df97f3cf94a573ad71
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:25:07 +0800

    Update ImageWithFallback.tsx

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -19,3 +19,4 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
     >
       <div className="flex items-center justify-center w-full h-full">
         <img src={ERROR_IMG_SRC} alt="Error loading image" {...rest} data-original-url={src} />
+      </div>
