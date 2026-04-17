a4f5835ef82a61460223a2a4c5538905c3fdb2fd
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:32:49 +0800

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -84,3 +84,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     <motion.div
                       initial={{ opacity: 0, y: -8, scale: 0.95 }}
                       animate={{ opacity: 1, y: 0, scale: 1 }}
+                      exit={{ opacity: 0, y: -8, scale: 0.95 }}
