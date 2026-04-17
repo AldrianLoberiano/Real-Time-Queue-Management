d1a60788d5bcb9f56611fd73dfa2ffe6c79996b4
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:25:22 +0800

    Update ImageWithFallback.tsx

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -24,3 +24,4 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
   ) : (
     <img src={src} alt={alt} className={className} style={style} {...rest} onError={handleError} />
   )
+}
