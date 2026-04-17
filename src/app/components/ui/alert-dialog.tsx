4fb6ddeb655f71ee6692850f5081d796debb84f5
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 22:02:21 +0800

    Update alert-dialog.tsx

--- src/app/components/ui/alert-dialog.tsx
+++ src/app/components/ui/alert-dialog.tsx
@@ -24,3 +24,4 @@ function AlertDialogPortal({
   ...props
 }: React.ComponentProps<typeof AlertDialogPrimitive.Portal>) {
   return (
+    <AlertDialogPrimitive.Portal data-slot="alert-dialog-portal" {...props} />
