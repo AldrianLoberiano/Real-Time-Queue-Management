9b70e6c04575b9034639401cdc2fd598cf2940bd
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:59:31 +0800

    Update accordion.tsx

--- src/app/components/ui/accordion.tsx
+++ src/app/components/ui/accordion.tsx
@@ -57,3 +57,4 @@ function AccordionContent({
       data-slot="accordion-content"
       className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
       {...props}
+    >
