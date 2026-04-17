bbf628b3725c4afa4751a0c46d68eee63023c12b
AldrianLoberiano <loberianorian@gmail.com>
2026-04-17 21:29:22 +0800

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -31,3 +31,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <span className="text-white font-semibold text-lg leading-none">QueueSmart</span>
                 <p className="text-blue-300 text-xs">Real-Time Queue Manager</p>
               </div>
+            </Link>
