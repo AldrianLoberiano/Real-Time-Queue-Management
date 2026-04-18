f44408def7f40f7e3f18b194fe382b0ea8c20af5
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:52:53 +0800

    Update context-menu.tsx

--- src/app/components/ui/context-menu.tsx
+++ src/app/components/ui/context-menu.tsx
@@ -212,3 +212,4 @@ function ContextMenuSeparator({
     <ContextMenuPrimitive.Separator
       data-slot="context-menu-separator"
       className={cn("bg-border -mx-1 my-1 h-px", className)}
+      {...props}
