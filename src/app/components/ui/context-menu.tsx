6f9ca040555cb4f54a7354b8f55fff701a29a21a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:31:55 +0800

    Update context-menu.tsx

--- src/app/components/ui/context-menu.tsx
+++ src/app/components/ui/context-menu.tsx
@@ -24,3 +24,4 @@ function ContextMenuGroup({
   ...props
 }: React.ComponentProps<typeof ContextMenuPrimitive.Group>) {
   return (
+    <ContextMenuPrimitive.Group data-slot="context-menu-group" {...props} />
