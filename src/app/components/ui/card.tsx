f687d09627fc25ff3a9d2cb42f0a3351b95f9772
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:34:54 +0800

    Update card.tsx

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -55,3 +55,4 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
       className={cn(
         "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
         className,
+      )}
