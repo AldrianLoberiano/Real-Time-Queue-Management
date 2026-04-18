db7781b63696272769d9eb5e0a0f4f63bda9b83e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:29:17 +0800

    Update card.tsx

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -8,3 +8,4 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card"
       className={cn(
         "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border",
+        className,
