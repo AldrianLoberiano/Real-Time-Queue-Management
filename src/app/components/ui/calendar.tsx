de84282955539451a44fb7876d23d15826b43b7c
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:24:07 +0800

    Update calendar.tsx

--- src/app/components/ui/calendar.tsx
+++ src/app/components/ui/calendar.tsx
@@ -52,3 +52,4 @@ function Calendar({
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
+          "day-outside text-muted-foreground aria-selected:text-muted-foreground",
