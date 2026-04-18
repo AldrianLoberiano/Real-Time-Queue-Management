a23c529a99b845d2b97393e701066a582f5012b6
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:10:29 +0800

    Update button.tsx

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -20,3 +20,4 @@ const buttonVariants = cva(
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
+      size: {
