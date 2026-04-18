42481c93d143fb94a8a6634722a0a5d9145ce62c
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 20:28:10 +0800

    Update breadcrumb.tsx

--- src/app/components/ui/breadcrumb.tsx
+++ src/app/components/ui/breadcrumb.tsx
@@ -26,3 +26,4 @@ function BreadcrumbItem({ className, ...props }: React.ComponentProps<"li">) {
     <li
       data-slot="breadcrumb-item"
       className={cn("inline-flex items-center gap-1.5", className)}
+      {...props}
