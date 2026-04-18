9ea1781d2c8409e9e4065209995101a3685f427f
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:04:32 +0800

    Update collapsible.tsx

--- src/app/components/ui/collapsible.tsx
+++ src/app/components/ui/collapsible.tsx
@@ -5,3 +5,4 @@ import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 function Collapsible({
   ...props
 }: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />;
