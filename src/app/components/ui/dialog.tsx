74c5dbc26af44925f08327f757f2e2ac1179333a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 23:00:26 +0800

    Update dialog.tsx

--- src/app/components/ui/dialog.tsx
+++ src/app/components/ui/dialog.tsx
@@ -78,3 +78,4 @@ function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="dialog-header"
       className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
       {...props}
+    />
