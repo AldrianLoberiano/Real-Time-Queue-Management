e5f358f71922437d9c322151cf713619de571fa7
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:21:44 +0800

    Update calendar.tsx

--- src/app/components/ui/calendar.tsx
+++ src/app/components/ui/calendar.tsx
@@ -27,3 +27,4 @@ function Calendar({
           buttonVariants({ variant: "outline" }),
           "size-7 bg-transparent p-0 opacity-50 hover:opacity-100",
         ),
+        nav_button_previous: "absolute left-1",
