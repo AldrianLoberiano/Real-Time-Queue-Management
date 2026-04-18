74d840269dfec274de87bea46bfee6d3ca6cb269
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:18:53 +0800

    Update command.tsx

--- src/app/components/ui/command.tsx
+++ src/app/components/ui/command.tsx
@@ -113,3 +113,4 @@ function CommandGroup({
       className={cn(
         "text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium",
         className,
+      )}
