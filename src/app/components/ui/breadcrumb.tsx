ff3e3fa54f810a84043598c8fcef35c5f066cde8
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 20:26:47 +0800

    Update breadcrumb.tsx

--- src/app/components/ui/breadcrumb.tsx
+++ src/app/components/ui/breadcrumb.tsx
@@ -16,3 +16,4 @@ function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
         "text-muted-foreground flex flex-wrap items-center gap-1.5 text-sm break-words sm:gap-2.5",
         className,
       )}
+      {...props}
