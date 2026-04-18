16e03be2375a161a8f6a3b68c80057700c024693
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:39:56 +0800

    Update carousel.tsx

--- src/app/components/ui/carousel.tsx
+++ src/app/components/ui/carousel.tsx
@@ -12,3 +12,4 @@ import { Button } from "./button";
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
 type CarouselOptions = UseCarouselParameters[0];
+type CarouselPlugin = UseCarouselParameters[1];
