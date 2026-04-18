ace977ed961dacc3923f1a485b39446918188303
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:14:56 +0800

    Update command.tsx

--- src/app/components/ui/command.tsx
+++ src/app/components/ui/command.tsx
@@ -68,3 +68,4 @@ function CommandInput({
         className={cn(
           "placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
           className,
+        )}
