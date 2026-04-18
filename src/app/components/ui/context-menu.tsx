3110b11f4c9a215e96bdc086bd577a81dbbd1c6c
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:44:12 +0800

    Update context-menu.tsx

--- src/app/components/ui/context-menu.tsx
+++ src/app/components/ui/context-menu.tsx
@@ -145,3 +145,4 @@ function ContextMenuCheckboxItem({
       data-slot="context-menu-checkbox-item"
       className={cn(
         "focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        className,
