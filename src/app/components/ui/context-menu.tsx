0afd77d101484a15502d94ceb0314ca255225b6d
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:51:02 +0800

    Update context-menu.tsx

--- src/app/components/ui/context-menu.tsx
+++ src/app/components/ui/context-menu.tsx
@@ -196,3 +196,4 @@ function ContextMenuLabel({
       data-slot="context-menu-label"
       data-inset={inset}
       className={cn(
+        "text-foreground px-2 py-1.5 text-sm font-medium data-[inset]:pl-8",
