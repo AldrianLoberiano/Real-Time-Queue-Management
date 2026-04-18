9365a06e0e4ae3f6dc983a957664d80e3787bb21
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:08:47 +0800

    Update button.tsx

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -15,4 +15,5 @@ const buttonVariants = cva(
         outline:
           "border bg-background text-foreground hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
