207b590795cd0241f41e27cea6d9baa19e8cc389
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:30:12 +0800

    Update card.tsx

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -10,3 +10,4 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
         "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border",
         className,
       )}
+      {...props}
