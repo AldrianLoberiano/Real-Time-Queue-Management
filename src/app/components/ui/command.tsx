c5b0a97ea928e5184941b544ab1716878e199264
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 22:14:23 +0800

    Update command.tsx

--- src/app/components/ui/command.tsx
+++ src/app/components/ui/command.tsx
@@ -62,3 +62,4 @@ function CommandInput({
       data-slot="command-input-wrapper"
       className="flex h-9 items-center gap-2 border-b px-3"
     >
+      <SearchIcon className="size-4 shrink-0 opacity-50" />
