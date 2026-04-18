b4a2adb5e0ede042e885a40d880701b70d98fcc8
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:34:20 +0800

    Update card.tsx

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -50,3 +50,4 @@ function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
 
 function CardAction({ className, ...props }: React.ComponentProps<"div">) {
   return (
+    <div
