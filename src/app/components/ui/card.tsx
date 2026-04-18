d48c2a1e8ea3051d06a1c846fe17bb4503f64980
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 21:36:49 +0800

    Update card.tsx

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -77,3 +77,4 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-footer"
       className={cn("flex items-center px-6 pb-6 [.border-t]:pt-6", className)}
       {...props}
+    />
