a4605eaf553acc23c135b748ecf94f23f5644dd3
AldrianLoberiano <loberianorian@gmail.com>
2026-04-18 20:33:50 +0800

    Update breadcrumb.tsx

--- src/app/components/ui/breadcrumb.tsx
+++ src/app/components/ui/breadcrumb.tsx
@@ -56,3 +56,4 @@ function BreadcrumbPage({ className, ...props }: React.ComponentProps<"span">) {
       role="link"
       aria-disabled="true"
       aria-current="page"
+      className={cn("text-foreground font-normal", className)}
