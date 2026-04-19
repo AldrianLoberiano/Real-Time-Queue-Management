9df0e2dbbb7602f57cf037f9524c7453a40d1485
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:37:36 +0800

    Update drawer.tsx

--- src/app/components/ui/drawer.tsx
+++ src/app/components/ui/drawer.tsx
@@ -26,3 +26,4 @@ function DrawerPortal({
 function DrawerClose({
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Close>) {
+  return <DrawerPrimitive.Close data-slot="drawer-close" {...props} />;
