dfb5d669e8f4c94117739092bd7bdb1ec05e4f85
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:40:45 +0800

    Update drawer.tsx

--- src/app/components/ui/drawer.tsx
+++ src/app/components/ui/drawer.tsx
@@ -87,3 +87,4 @@ function DrawerFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="drawer-footer"
       className={cn("mt-auto flex flex-col gap-2 p-4", className)}
+      {...props}
