4336a22e88031e792d6571b94fba59eeaf48b0ed
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:43:41 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -148,3 +148,4 @@ function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
     <p
       data-slot="form-message"
       id={formMessageId}
+      className={cn("text-destructive text-sm", className)}
