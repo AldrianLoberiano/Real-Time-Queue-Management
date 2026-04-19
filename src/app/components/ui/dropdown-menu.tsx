cae3984e386c23a585fbc29d6f9416d15e7947be
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:55:37 +0800

    Update dropdown-menu.tsx

--- src/app/components/ui/dropdown-menu.tsx
+++ src/app/components/ui/dropdown-menu.tsx
@@ -169,3 +169,4 @@ function DropdownMenuSeparator({
 }: React.ComponentProps<typeof DropdownMenuPrimitive.Separator>) {
   return (
     <DropdownMenuPrimitive.Separator
+      data-slot="dropdown-menu-separator"
