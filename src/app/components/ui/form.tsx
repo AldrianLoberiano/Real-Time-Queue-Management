5f2face0291133468676a84dd7883d8977ca23c1
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:41:28 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -106,3 +106,4 @@ function FormLabel({
 
 function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
   const { error, formItemId, formDescriptionId, formMessageId } =
+    useFormField();
