eeef7a77b7b98a401b4f2486c12ef09805362016
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:45:20 +0800

    Update dropdown-menu.tsx

--- src/app/components/ui/dropdown-menu.tsx
+++ src/app/components/ui/dropdown-menu.tsx
@@ -4,3 +4,4 @@ import * as React from "react";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react";
 
+import { cn } from "./utils";
