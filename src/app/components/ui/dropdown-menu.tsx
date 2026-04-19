ae79f25244881c05a0f948ce382c513c98e0de8c
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:52:00 +0800

    Update dropdown-menu.tsx

--- src/app/components/ui/dropdown-menu.tsx
+++ src/app/components/ui/dropdown-menu.tsx
@@ -100,3 +100,4 @@ function DropdownMenuCheckboxItem({
     >
       <span className="pointer-events-none absolute left-2 flex size-3.5 items-center justify-center">
         <DropdownMenuPrimitive.ItemIndicator>
+          <CheckIcon className="size-4" />
