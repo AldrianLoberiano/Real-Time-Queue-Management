6a5d7660550844923e4e33b2b31c7bc1d43ce38e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:44:57 +0800

    Update hover-card.tsx

--- src/app/components/ui/hover-card.tsx
+++ src/app/components/ui/hover-card.tsx
@@ -15,3 +15,4 @@ function HoverCardTrigger({
   ...props
 }: React.ComponentProps<typeof HoverCardPrimitive.Trigger>) {
   return (
+    <HoverCardPrimitive.Trigger data-slot="hover-card-trigger" {...props} />
