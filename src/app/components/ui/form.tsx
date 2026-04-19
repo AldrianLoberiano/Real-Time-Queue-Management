07d66fc298c91429a822139be58879ee0009549a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:43:19 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -141,3 +141,4 @@ function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
   const body = error ? String(error?.message ?? "") : props.children;
 
   if (!body) {
+    return null;
