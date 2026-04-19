cf5015234bdfe5e11b52557aa93651e5d6c67ceb
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:40:26 +0800

    Update drawer.tsx

--- src/app/components/ui/drawer.tsx
+++ src/app/components/ui/drawer.tsx
@@ -78,3 +78,4 @@ function DrawerHeader({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="drawer-header"
       className={cn("flex flex-col gap-1.5 p-4", className)}
       {...props}
+    />
