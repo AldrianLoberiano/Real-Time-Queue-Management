224a654ba796e42f35fc7042f676fc7008670078
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:03:52 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -81,3 +81,4 @@ function FormItem({ className, ...props }: React.ComponentProps<"div">) {
       <div
         data-slot="form-item"
         className={cn("grid gap-2", className)}
+        {...props}
