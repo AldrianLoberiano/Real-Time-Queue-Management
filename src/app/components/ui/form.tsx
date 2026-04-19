910245e7ed836cb512054a00be0b01d3ab6aa5e7
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:42:41 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -126,3 +126,4 @@ function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
 function FormDescription({ className, ...props }: React.ComponentProps<"p">) {
   const { formDescriptionId } = useFormField();
 
+  return (
