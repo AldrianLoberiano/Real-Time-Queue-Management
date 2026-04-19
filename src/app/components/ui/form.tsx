aad17d2ac08f9db0b9f9f76d199c63ff72d491ed
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:03:06 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -61,3 +61,4 @@ const useFormField = () => {
     formItemId: `${id}-form-item`,
     formDescriptionId: `${id}-form-item-description`,
     formMessageId: `${id}-form-item-message`,
+    ...fieldState,
