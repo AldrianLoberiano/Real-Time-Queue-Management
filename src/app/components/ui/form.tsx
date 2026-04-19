aee3a545f97b5a614c437a5bc2b848e65bbc5cdd
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:02:40 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -50,3 +50,4 @@ const useFormField = () => {
   const fieldState = getFieldState(fieldContext.name, formState);
 
   if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>");
