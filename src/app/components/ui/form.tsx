53709b55dcb8713c033a425be4be02693f376d5a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 20:03:03 +0800

    Update form.tsx

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -60,3 +60,4 @@ const useFormField = () => {
     name: fieldContext.name,
     formItemId: `${id}-form-item`,
     formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
