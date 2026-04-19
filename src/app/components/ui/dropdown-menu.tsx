b7e347c393a66d340153288a9669afc9ea39d1ff
AldrianLoberiano <loberianorian@gmail.com>
2026-04-19 19:49:22 +0800

    Update dropdown-menu.tsx

--- src/app/components/ui/dropdown-menu.tsx
+++ src/app/components/ui/dropdown-menu.tsx
@@ -55,3 +55,4 @@ function DropdownMenuGroup({
   ...props
 }: React.ComponentProps<typeof DropdownMenuPrimitive.Group>) {
   return (
+    <DropdownMenuPrimitive.Group data-slot="dropdown-menu-group" {...props} />
