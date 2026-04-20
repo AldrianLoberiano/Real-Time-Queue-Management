8ba70e89e47461ef62cc1641e47ce8898ace8bed
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:33:32 +0800

    Update navigation-menu.tsx

--- src/app/components/ui/navigation-menu.tsx
+++ src/app/components/ui/navigation-menu.tsx
@@ -2,3 +2,4 @@ import * as React from "react";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
 import { ChevronDownIcon } from "lucide-react";
+import { cn } from "./utils";
