7c78ceaae6aebbc0bb1a9aa76664f2319c76e024
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:25:18 +0800

    Update menubar.tsx

--- src/app/components/ui/menubar.tsx
+++ src/app/components/ui/menubar.tsx
@@ -153,3 +153,4 @@ function MenubarRadioItem({
     >
       <span className="pointer-events-none absolute left-2 flex size-3.5 items-center justify-center">
         <MenubarPrimitive.ItemIndicator>
+          <CircleIcon className="size-2 fill-current" />
