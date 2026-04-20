da2e3ba2f4c37d7eeec06eb5ef9f2794135a2875
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:22:54 +0800

    Update menubar.tsx

--- src/app/components/ui/menubar.tsx
+++ src/app/components/ui/menubar.tsx
@@ -127,3 +127,4 @@ function MenubarCheckboxItem({
       checked={checked}
       {...props}
     >
+      <span className="pointer-events-none absolute left-2 flex size-3.5 items-center justify-center">
