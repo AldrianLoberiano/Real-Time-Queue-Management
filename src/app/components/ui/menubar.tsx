6a4d483fad38c4f624822bec0639daf1f4dc694e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:30:16 +0800

    Update menubar.tsx

--- src/app/components/ui/menubar.tsx
+++ src/app/components/ui/menubar.tsx
@@ -232,3 +232,4 @@ function MenubarSubTrigger({
         "focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center rounded-sm px-2 py-1.5 text-sm outline-none select-none data-[inset]:pl-8",
         className,
       )}
+      {...props}
