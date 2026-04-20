33d668c2d3d16b9bdf20d9b3d4f1fc2eed7902bc
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:35:28 +0800

    Update navigation-menu.tsx

--- src/app/components/ui/navigation-menu.tsx
+++ src/app/components/ui/navigation-menu.tsx
@@ -19,3 +19,4 @@ function NavigationMenu({
       data-viewport={viewport}
       className={cn(
         "group/navigation-menu relative flex max-w-max flex-1 items-center justify-center",
+        className,
