efc6d53bcd713fbf0ea70bbf24575de5114f6661
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 19:38:17 +0800

    Update input-otp.tsx

--- src/app/components/ui/input-otp.tsx
+++ src/app/components/ui/input-otp.tsx
@@ -20,3 +20,4 @@ function InputOTP({
         "flex items-center gap-2 has-disabled:opacity-50",
         containerClassName,
       )}
+      className={cn("disabled:cursor-not-allowed", className)}
