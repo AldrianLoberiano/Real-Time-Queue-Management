11b33e5a5cb2aaced45cde460377ef303f3acafc
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 19:42:14 +0800

    Update input-otp.tsx

--- src/app/components/ui/input-otp.tsx
+++ src/app/components/ui/input-otp.tsx
@@ -70,3 +70,4 @@ function InputOTPSeparator({ ...props }: React.ComponentProps<"div">) {
   return (
     <div data-slot="input-otp-separator" role="separator" {...props}>
       <MinusIcon />
+    </div>
