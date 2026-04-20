5cc36cb19cab75bdd3c913d3da4cc3b777c1a030
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:15:57 +0800

    Update menubar.tsx

--- src/app/components/ui/menubar.tsx
+++ src/app/components/ui/menubar.tsx
@@ -57,3 +57,4 @@ function MenubarTrigger({
       data-slot="menubar-trigger"
       className={cn(
         "focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex items-center rounded-sm px-2 py-1 text-sm font-medium outline-hidden select-none",
+        className,
