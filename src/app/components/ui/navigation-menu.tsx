bf8a3a0bca9a244e8dac4c82c04f36193d7132a2
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 20:43:49 +0800

    Update navigation-menu.tsx

--- src/app/components/ui/navigation-menu.tsx
+++ src/app/components/ui/navigation-menu.tsx
@@ -71,3 +71,4 @@ function NavigationMenuTrigger({
     <NavigationMenuPrimitive.Trigger
       data-slot="navigation-menu-trigger"
       className={cn(navigationMenuTriggerStyle(), "group", className)}
+      {...props}
