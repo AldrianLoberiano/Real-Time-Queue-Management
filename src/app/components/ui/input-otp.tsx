c48a83bda80112cc615535a7fefc78c97597c9bb
AldrianLoberiano <loberianorian@gmail.com>
2026-04-20 19:41:46 +0800

    Update input-otp.tsx

--- src/app/components/ui/input-otp.tsx
+++ src/app/components/ui/input-otp.tsx
@@ -61,3 +61,4 @@ function InputOTPSlot({
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
           <div className="animate-caret-blink bg-foreground h-4 w-px duration-1000" />
         </div>
+      )}
