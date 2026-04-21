67d50bc30791dd330fe091afecf7e6f2742235f6
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:05:40 +0800

    Update radio-group.tsx

--- src/app/components/ui/radio-group.tsx
+++ src/app/components/ui/radio-group.tsx
@@ -37,3 +37,4 @@ function RadioGroupItem({
         className="relative flex items-center justify-center"
       >
         <CircleIcon className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />
+      </RadioGroupPrimitive.Indicator>
