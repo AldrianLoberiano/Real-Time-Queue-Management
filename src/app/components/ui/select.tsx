8a8f8e552862aded26bd8ec0d2329b40d62e20ed
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:17:42 +0800

    Update select.tsx

--- src/app/components/ui/select.tsx
+++ src/app/components/ui/select.tsx
@@ -19,3 +19,4 @@ function Select({
 function SelectGroup({
   ...props
 }: React.ComponentProps<typeof SelectPrimitive.Group>) {
+  return <SelectPrimitive.Group data-slot="select-group" {...props} />;
