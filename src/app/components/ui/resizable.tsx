aab78676915194d0242a71567b329d440149f1a7
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:10:31 +0800

    Update resizable.tsx

--- src/app/components/ui/resizable.tsx
+++ src/app/components/ui/resizable.tsx
@@ -25,3 +25,4 @@ function ResizablePanelGroup({
 function ResizablePanel({
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.Panel>) {
+  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />;
