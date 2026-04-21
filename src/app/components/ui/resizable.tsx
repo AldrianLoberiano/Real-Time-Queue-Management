8652c320395e1194648d6ec7a90e9be0792e69e8
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:11:24 +0800

    Update resizable.tsx

--- src/app/components/ui/resizable.tsx
+++ src/app/components/ui/resizable.tsx
@@ -47,4 +47,5 @@ function ResizableHandle({
       {withHandle && (
         <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
           <GripVerticalIcon className="size-2.5" />
+        </div>
       )}
