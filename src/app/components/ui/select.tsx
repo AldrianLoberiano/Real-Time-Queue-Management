901ae439eff12ede505b6a7a01ff02de5b283e8f
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:20:48 +0800

    Update select.tsx

--- src/app/components/ui/select.tsx
+++ src/app/components/ui/select.tsx
@@ -70,4 +70,5 @@ function SelectContent({
             "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
           className,
         )}
+        position={position}
         <SelectScrollUpButton />
