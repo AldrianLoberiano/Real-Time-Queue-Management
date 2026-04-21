78ec522bdd89f331e4797192dddc08a770ac5323
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:15:59 +0800

    Update scroll-area.tsx

--- src/app/components/ui/scroll-area.tsx
+++ src/app/components/ui/scroll-area.tsx
@@ -41,3 +41,4 @@ function ScrollBar({
         "flex touch-none p-px transition-colors select-none",
         orientation === "vertical" &&
           "h-full w-2.5 border-l border-l-transparent",
+        orientation === "horizontal" &&
