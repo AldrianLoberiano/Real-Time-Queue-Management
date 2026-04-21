366acf0bb1893f59e089274f8e9856c591ee1dcc
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 19:42:34 +0800

    Update navigation-menu.tsx

--- src/app/components/ui/navigation-menu.tsx
+++ src/app/components/ui/navigation-menu.tsx
@@ -147,3 +147,4 @@ function NavigationMenuIndicator({
       className={cn(
         "data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden",
         className,
+      )}
