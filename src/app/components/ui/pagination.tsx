7a789a57dfba5c77f1803a4a74a7caa0ab4fbdad
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 19:47:01 +0800

    Update pagination.tsx

--- src/app/components/ui/pagination.tsx
+++ src/app/components/ui/pagination.tsx
@@ -40,3 +40,4 @@ function PaginationItem({ ...props }: React.ComponentProps<"li">) {
 type PaginationLinkProps = {
   isActive?: boolean;
 } & Pick<React.ComponentProps<typeof Button>, "size"> &
+  React.ComponentProps<"a">;
