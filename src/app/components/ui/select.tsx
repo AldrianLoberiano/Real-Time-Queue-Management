cf780b6bcc1e3100904b32d2fee8bdd38a9d4288
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:24:19 +0800

    Update select.tsx

--- src/app/components/ui/select.tsx
+++ src/app/components/ui/select.tsx
@@ -134,3 +134,4 @@ function SelectSeparator({
     <SelectPrimitive.Separator
       data-slot="select-separator"
       className={cn("bg-border pointer-events-none -mx-1 my-1 h-px", className)}
+      {...props}
