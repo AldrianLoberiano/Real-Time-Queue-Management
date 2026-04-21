514fd2e9dbbc0566bacbe72cfa19665638177fe6
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:21:20 +0800

    Update select.tsx

--- src/app/components/ui/select.tsx
+++ src/app/components/ui/select.tsx
@@ -79,3 +79,4 @@ function SelectContent({
             "p-1",
             position === "popper" &&
               "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1",
+          )}
