c555606a8fd9940a19a39d9dc61fcf780abba938
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:01:07 +0800

    Update popover.tsx

--- src/app/components/ui/popover.tsx
+++ src/app/components/ui/popover.tsx
@@ -42,3 +42,4 @@ function PopoverContent({
 function PopoverAnchor({
   ...props
 }: React.ComponentProps<typeof PopoverPrimitive.Anchor>) {
+  return <PopoverPrimitive.Anchor data-slot="popover-anchor" {...props} />;
