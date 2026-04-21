9aa0cbb4777e1191162ef0eb23554ee3b8e02ae7
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:23:18 +0800

    Update select.tsx

--- src/app/components/ui/select.tsx
+++ src/app/components/ui/select.tsx
@@ -118,3 +118,4 @@ function SelectItem({
     >
       <span className="absolute right-2 flex size-3.5 items-center justify-center">
         <SelectPrimitive.ItemIndicator>
+          <CheckIcon className="size-4" />
