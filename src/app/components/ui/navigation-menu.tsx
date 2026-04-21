9fc459228329dd8155c52edc9ed2743c9eb01e52
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 19:43:23 +0800

    Update navigation-menu.tsx

--- src/app/components/ui/navigation-menu.tsx
+++ src/app/components/ui/navigation-menu.tsx
@@ -153,3 +153,4 @@ function NavigationMenuIndicator({
       <div className="bg-border relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm shadow-md" />
     </NavigationMenuPrimitive.Indicator>
   );
+}
