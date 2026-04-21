96b237bd65f4d7330b103f7a60b3aba59f5f90ac
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:24:51 +0800

    Update select.tsx

--- src/app/components/ui/select.tsx
+++ src/app/components/ui/select.tsx
@@ -147,3 +147,4 @@ function SelectScrollUpButton({
     <SelectPrimitive.ScrollUpButton
       data-slot="select-scroll-up-button"
       className={cn(
+        "flex cursor-default items-center justify-center py-1",
