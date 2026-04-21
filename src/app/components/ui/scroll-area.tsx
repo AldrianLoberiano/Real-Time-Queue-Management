5cbec88602e21ca98ef0af0a6227fe982e733c62
AldrianLoberiano <loberianorian@gmail.com>
2026-04-21 20:14:55 +0800

    Update scroll-area.tsx

--- src/app/components/ui/scroll-area.tsx
+++ src/app/components/ui/scroll-area.tsx
@@ -20,3 +20,4 @@ function ScrollArea({
         data-slot="scroll-area-viewport"
         className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
       >
+        {children}
