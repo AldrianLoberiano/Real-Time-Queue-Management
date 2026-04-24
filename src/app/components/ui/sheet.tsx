ed1865350b02baaa98fb4ced4c5a813d5a9af63e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-24 14:18:07 +0800

    Update sheet.tsx

--- src/app/components/ui/sheet.tsx
+++ src/app/components/ui/sheet.tsx
@@ -64,3 +64,4 @@ function SheetContent({
           side === "left" &&
             "data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm",
           side === "top" &&
+            "data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b",
