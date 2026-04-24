42020abd00b20f6a808c4ae823cccf524366cd62
AldrianLoberiano <loberianorian@gmail.com>
2026-04-24 14:19:09 +0800

    Update sheet.tsx

--- src/app/components/ui/sheet.tsx
+++ src/app/components/ui/sheet.tsx
@@ -87,3 +87,4 @@ function SheetHeader({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="sheet-header"
       className={cn("flex flex-col gap-1.5 p-4", className)}
       {...props}
+    />
