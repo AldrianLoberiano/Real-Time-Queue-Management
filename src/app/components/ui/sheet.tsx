ab5f9874ad6467e896caaaf5bbd5f2c926757e55
AldrianLoberiano <loberianorian@gmail.com>
2026-04-24 14:17:45 +0800

    Update sheet.tsx

--- src/app/components/ui/sheet.tsx
+++ src/app/components/ui/sheet.tsx
@@ -59,3 +59,4 @@ function SheetContent({
         data-slot="sheet-content"
         className={cn(
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-4 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+          side === "right" &&
