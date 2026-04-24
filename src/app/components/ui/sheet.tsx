775b13d3944a44448a8787fcc183116043210b05
AldrianLoberiano <loberianorian@gmail.com>
2026-04-24 14:19:32 +0800

    Update sheet.tsx

--- src/app/components/ui/sheet.tsx
+++ src/app/components/ui/sheet.tsx
@@ -96,3 +96,4 @@ function SheetFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="sheet-footer"
       className={cn("mt-auto flex flex-col gap-2 p-4", className)}
+      {...props}
