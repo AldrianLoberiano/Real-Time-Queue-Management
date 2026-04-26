ba2050aff8207b5f6331af21e31fba7b15e1acc1
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:32:24 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -140,3 +140,4 @@ function SidebarProvider({
           }
           className={cn(
             "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
+            className,
