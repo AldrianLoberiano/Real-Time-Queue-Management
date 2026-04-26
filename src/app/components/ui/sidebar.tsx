c72e9ac5abf197a23137f27551878bfb965bdb47
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:27:39 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -72,3 +72,4 @@ function SidebarProvider({
   // This is the internal state of the sidebar.
   // We use openProp and setOpenProp for control from outside the component.
   const [_open, _setOpen] = React.useState(defaultOpen);
+  const open = openProp ?? _open;
