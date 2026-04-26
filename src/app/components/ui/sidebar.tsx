426210ad3811055260e3ed02f63826dc4fd5dae6
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:42:09 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -298,4 +298,5 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
         "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
         "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
         className,
+      )}
       {...props}
