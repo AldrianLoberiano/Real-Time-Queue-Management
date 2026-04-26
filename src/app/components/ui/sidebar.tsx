437c78409e72964d1d21d24498b2499f630ed6b9
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:29:30 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -98,3 +98,4 @@ function SidebarProvider({
     const handleKeyDown = (event: KeyboardEvent) => {
       if (
         event.key === SIDEBAR_KEYBOARD_SHORTCUT &&
+        (event.metaKey || event.ctrlKey)
