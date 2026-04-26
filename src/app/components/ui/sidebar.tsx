a5dd9b6314a2f2dfd8367f957bb09d425c83bb4b
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:30:30 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -112,3 +112,4 @@ function SidebarProvider({
   // We add a state so that we can do data-state="expanded" or "collapsed".
   // This makes it easier to style the sidebar with Tailwind classes.
   const state = open ? "expanded" : "collapsed";
+
