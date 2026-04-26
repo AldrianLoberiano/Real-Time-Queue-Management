8c0d3e0496ba5a80d48d17a8c1079952c4d71bd9
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:28:13 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -85,3 +85,4 @@ function SidebarProvider({
       // This sets the cookie to keep the sidebar state.
       document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
     },
+    [setOpenProp, open],
