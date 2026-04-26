87f16939343bbd966f2a2382fdcfcfcf5ae9feb9
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:36:55 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -221,3 +221,4 @@ function Sidebar({
           "relative w-(--sidebar-width) bg-transparent transition-[width] duration-200 ease-linear",
           "group-data-[collapsible=offcanvas]:w-0",
           "group-data-[side=right]:rotate-180",
+          variant === "floating" || variant === "inset"
