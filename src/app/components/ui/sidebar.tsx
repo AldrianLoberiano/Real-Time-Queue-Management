174b7b6fd203d15699015320b11e55b92a1e6899
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:25:25 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -47,3 +47,4 @@ const SidebarContext = React.createContext<SidebarContextProps | null>(null);
 function useSidebar() {
   const context = React.useContext(SidebarContext);
   if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.");
