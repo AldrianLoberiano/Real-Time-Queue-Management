7e0e067dba9cbb47d458b433aad8a1924f8ae8d1
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:27:33 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -70,3 +70,4 @@ function SidebarProvider({
   const [openMobile, setOpenMobile] = React.useState(false);
 
   // This is the internal state of the sidebar.
+  // We use openProp and setOpenProp for control from outside the component.
