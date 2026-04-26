d9be1a7bbce442b8f1ca12e35f5a8eda8dbeb3a4
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:35:52 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -199,3 +199,4 @@ function Sidebar({
             <SheetTitle>Sidebar</SheetTitle>
             <SheetDescription>Displays the mobile sidebar.</SheetDescription>
           </SheetHeader>
+          <div className="flex h-full w-full flex-col">{children}</div>
