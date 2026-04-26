89c9c02dc5464651c1b317fd32a0e24baa8aa400
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:35:24 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -190,3 +190,4 @@ function Sidebar({
           className="bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
+              "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
