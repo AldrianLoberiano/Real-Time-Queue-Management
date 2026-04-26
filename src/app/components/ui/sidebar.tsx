fb873d52e3b0659589b30f9b3ca1d966da12a845
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:41:11 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -286,3 +286,4 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
     <button
       data-sidebar="rail"
       data-slot="sidebar-rail"
+      aria-label="Toggle Sidebar"
