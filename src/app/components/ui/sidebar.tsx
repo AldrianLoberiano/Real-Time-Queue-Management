b5bc1ceaf428a7a7ae82dfc0c5ea4383099c80af
AldrianLoberiano <loberianorian@gmail.com>
2026-04-26 12:36:24 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -210,3 +210,4 @@ function Sidebar({
       className="group peer text-sidebar-foreground hidden md:block"
       data-state={state}
       data-collapsible={state === "collapsed" ? collapsible : ""}
+      data-variant={variant}
