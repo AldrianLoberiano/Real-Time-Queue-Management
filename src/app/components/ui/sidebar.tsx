26fd08e7268c60df36fa1361e1d66e438609dce2
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:47:39 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -690,3 +690,4 @@ function SidebarMenuSubButton({
         "data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground",
         size === "sm" && "text-xs",
         size === "md" && "text-sm",
+        "group-data-[collapsible=icon]:hidden",
