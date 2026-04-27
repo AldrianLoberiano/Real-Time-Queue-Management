724e9519f878cc6f6efd8167e5ec023a66cf0ac3
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:36:55 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -517,3 +517,4 @@ function SidebarMenuButton({
       data-sidebar="menu-button"
       data-size={size}
       data-active={isActive}
+      className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
