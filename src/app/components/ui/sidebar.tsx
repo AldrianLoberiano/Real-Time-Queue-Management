f2d3eacf1c8c2f2ac564e2d4ccc5bf51482ebeff
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:16:36 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -339,3 +339,4 @@ function SidebarHeader({ className, ...props }: React.ComponentProps<"div">) {
       data-sidebar="header"
       className={cn("flex flex-col gap-2 p-2", className)}
       {...props}
+    />
