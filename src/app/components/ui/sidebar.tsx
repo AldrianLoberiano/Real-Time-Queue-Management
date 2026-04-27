11dbcdc1cf2fb07754eb45f0c92361e50d9256c4
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:34:40 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -483,3 +483,4 @@ const sidebarMenuButtonVariants = cva(
           "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
       },
       size: {
+        default: "h-8 text-sm",
