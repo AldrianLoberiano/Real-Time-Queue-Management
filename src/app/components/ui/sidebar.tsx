5f0d23d4781fd59635bdc7ab72f90048e486276e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:17:30 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -349,3 +349,4 @@ function SidebarFooter({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="sidebar-footer"
       data-sidebar="footer"
       className={cn("flex flex-col gap-2 p-2", className)}
+      {...props}
