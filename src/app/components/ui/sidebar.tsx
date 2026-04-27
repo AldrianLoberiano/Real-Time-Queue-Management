fe4679d50d023685ab5f6ee61a19ad7a6b486894
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:40:41 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -568,3 +568,4 @@ function SidebarMenuAction({
         "peer-data-[size=default]/menu-button:top-1.5",
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
+        showOnHover &&
