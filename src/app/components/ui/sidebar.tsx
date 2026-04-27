8cdd802d52e4fb5a864f78bb362716ec147849f2
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:26:17 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -388,3 +388,4 @@ function SidebarGroup({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="sidebar-group"
       data-sidebar="group"
       className={cn("relative flex w-full min-w-0 flex-col p-2", className)}
+      {...props}
