9e1deafe6433deb4f464ecbaa846d3d20d26fa1d
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:31:50 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -456,3 +456,4 @@ function SidebarMenu({ className, ...props }: React.ComponentProps<"ul">) {
     <ul
       data-slot="sidebar-menu"
       data-sidebar="menu"
+      className={cn("flex w-full min-w-0 flex-col gap-1", className)}
