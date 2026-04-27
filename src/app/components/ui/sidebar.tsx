18b31bcc5fcbe86c6db0a5633c31515044c3b87e
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:40:51 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -572,3 +572,4 @@ function SidebarMenuAction({
           "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
         className,
       )}
+      {...props}
