a3c98b64b9116baf606c8f6d3a9fa59fe8941412
AldrianLoberiano <loberianorian@gmail.com>
2026-04-27 22:34:46 +0800

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -484,4 +484,5 @@ const sidebarMenuButtonVariants = cva(
       },
       size: {
         default: "h-8 text-sm",
+        sm: "h-7 text-xs",
         lg: "h-12 text-sm group-data-[collapsible=icon]:p-0!",
