c64a6dda6f83642ac673b3585acbb0b375c13c5a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 22:08:26 +0800

    Update QueueContext.tsx

--- src/app/context/QueueContext.tsx
+++ src/app/context/QueueContext.tsx
@@ -3,3 +3,4 @@ import React, { createContext, useContext, useState, useEffect, useCallback, use
 export type PriorityType = 'regular' | 'vip' | 'senior';
 export type StatusType = 'waiting' | 'serving' | 'done' | 'skipped';
 
+export interface QueueItem {
