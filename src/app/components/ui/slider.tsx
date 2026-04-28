6e771af68ba72d1df65882228230c1b71ab39e8a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:17:09 +0800

    Update slider.tsx

--- src/app/components/ui/slider.tsx
+++ src/app/components/ui/slider.tsx
@@ -45,3 +45,4 @@ function Slider({
         <SliderPrimitive.Range
           data-slot="slider-range"
           className={cn(
+            "bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full",
