18a9609c7ff4dfc291e8e495009a9bd5c2e7cd55
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:14:39 +0800

    Update slider.tsx

--- src/app/components/ui/slider.tsx
+++ src/app/components/ui/slider.tsx
@@ -41,3 +41,4 @@ function Slider({
         className={cn(
           "bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-4 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5",
         )}
+      >
