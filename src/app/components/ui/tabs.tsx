e8f73b7e508b3a6d1b70dbd7e928a692cdfd9b81
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:35:28 +0800

    Update tabs.tsx

--- src/app/components/ui/tabs.tsx
+++ src/app/components/ui/tabs.tsx
@@ -26,3 +26,4 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
+        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px] flex",
