d6104df35fd7353dec27eaaf72ecaf677a2df306
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:52:47 +0800

    Update toggle.tsx

--- src/app/components/ui/toggle.tsx
+++ src/app/components/ui/toggle.tsx
@@ -4,3 +4,4 @@ import * as React from "react";
 import * as TogglePrimitive from "@radix-ui/react-toggle";
 import { cva, type VariantProps } from "class-variance-authority";
 
+import { cn } from "./utils";
