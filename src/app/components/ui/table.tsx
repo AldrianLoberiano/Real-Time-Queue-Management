e9535ab687531536f764a11c774d651d141280aa
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:27:10 +0800

    Update table.tsx

--- src/app/components/ui/table.tsx
+++ src/app/components/ui/table.tsx
@@ -46,3 +46,4 @@ function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
       className={cn(
         "bg-muted/50 border-t font-medium [&>tr]:last:border-b-0",
         className,
+      )}
