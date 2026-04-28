fe413c94663686903db37507812cc365e9fedc8a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:29:03 +0800

    Update table.tsx

--- src/app/components/ui/table.tsx
+++ src/app/components/ui/table.tsx
@@ -73,3 +73,4 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
         "text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
         className,
       )}
+      {...props}
