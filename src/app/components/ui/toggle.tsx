c7110a72898e3bbd2caba469c2a63d0c8e59e548
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:53:44 +0800

    Update toggle.tsx

--- src/app/components/ui/toggle.tsx
+++ src/app/components/ui/toggle.tsx
@@ -16,3 +16,4 @@ const toggleVariants = cva(
           "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
       },
       size: {
+        default: "h-9 px-2 min-w-9",
