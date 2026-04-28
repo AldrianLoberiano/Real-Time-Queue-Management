0f3dba1ef1105f36426806eb72c58f60b2808cad
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:19:27 +0800

    Update sonner.tsx

--- src/app/components/ui/sonner.tsx
+++ src/app/components/ui/sonner.tsx
@@ -4,4 +4,5 @@ import { useTheme } from "next-themes";
 import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
 
