4d1cc1c59828706bf6bbac299d718649fe3154d2
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:14:41 +0800

    Update slider.tsx

--- src/app/components/ui/slider.tsx
+++ src/app/components/ui/slider.tsx
@@ -42,3 +42,4 @@ function Slider({
           "bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-4 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5",
         )}
       >
+        <SliderPrimitive.Range
