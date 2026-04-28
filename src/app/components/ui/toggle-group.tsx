a80ced39bf6899af55527e098cd569ed4ae43aeb
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:44:44 +0800

    Update toggle-group.tsx

--- src/app/components/ui/toggle-group.tsx
+++ src/app/components/ui/toggle-group.tsx
@@ -29,3 +29,4 @@ function ToggleGroup({
       data-size={size}
       className={cn(
         "group/toggle-group flex w-fit items-center rounded-md data-[variant=outline]:shadow-xs",
+        className,
