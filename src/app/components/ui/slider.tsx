062ab414f93ce5854b321e479f8a43d96e28d871
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:17:17 +0800

    Update slider.tsx

--- src/app/components/ui/slider.tsx
+++ src/app/components/ui/slider.tsx
@@ -48,3 +48,4 @@ function Slider({
             "bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full",
           )}
         />
+      </SliderPrimitive.Track>
