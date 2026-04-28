034c822a5f9111d08eb2d15f3f100b5ca7a4758c
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:27:43 +0800

    Update table.tsx

--- src/app/components/ui/table.tsx
+++ src/app/components/ui/table.tsx
@@ -55,3 +55,4 @@ function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
 function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
   return (
     <tr
+      data-slot="table-row"
