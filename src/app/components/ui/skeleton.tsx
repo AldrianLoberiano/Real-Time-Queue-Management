6f02985ba8dc22ce59aa063abb3407b17ae9743a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:10:04 +0800

    Update skeleton.tsx

--- src/app/components/ui/skeleton.tsx
+++ src/app/components/ui/skeleton.tsx
@@ -6,3 +6,4 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="skeleton"
       className={cn("bg-accent animate-pulse rounded-md", className)}
       {...props}
+    />
