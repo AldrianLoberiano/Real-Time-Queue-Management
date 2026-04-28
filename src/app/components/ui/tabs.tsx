78c163e2843ae117daa7fe3e5b9642cdfa97bb4f
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:35:57 +0800

    Update tabs.tsx

--- src/app/components/ui/tabs.tsx
+++ src/app/components/ui/tabs.tsx
@@ -28,3 +28,4 @@ function TabsList({
       className={cn(
         "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px] flex",
         className,
+      )}
