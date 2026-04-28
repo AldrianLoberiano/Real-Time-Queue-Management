a9776e218fc1f8145d0f79f6227e768b9d50c292
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:23:17 +0800

    Update switch.tsx

--- src/app/components/ui/switch.tsx
+++ src/app/components/ui/switch.tsx
@@ -24,3 +24,4 @@ function Switch({
           "bg-card dark:data-[state=unchecked]:bg-card-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0",
         )}
       />
+    </SwitchPrimitive.Root>
