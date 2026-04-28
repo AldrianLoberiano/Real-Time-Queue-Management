089a147ef4e40cb7ecbff8bb2ad09dc03eb2d039
AldrianLoberiano <loberianorian@gmail.com>
2026-04-28 21:41:37 +0800

    Update toggle-group.tsx

--- src/app/components/ui/toggle-group.tsx
+++ src/app/components/ui/toggle-group.tsx
@@ -5,3 +5,4 @@ import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { type VariantProps } from "class-variance-authority";
 
 import { cn } from "./utils";
+import { toggleVariants } from "./toggle";
