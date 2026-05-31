60eb9ce30572c98b04d0666553eb5c615465fa63
AldrianLoberiano <loberianorian@gmail.com>
2026-05-31 20:51:14 +0800

    Update QueueContext.tsx

--- src/app/context/QueueContext.tsx
+++ src/app/context/QueueContext.tsx
@@ -168,3 +168,4 @@ export function QueueProvider({ children }: { children: React.ReactNode }) {
         if (pDiff !== 0) return pDiff;
         return a.createdAt.getTime() - b.createdAt.getTime();
       });
+    return sorted.findIndex(i => i.id === id) + 1;
