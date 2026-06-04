3617a9838725da6037010d31f2321288d4d3eeaf
AldrianLoberiano <loberianorian@gmail.com>
2026-06-04 22:42:47 +0800

    Update AdminAnalyticsPage.tsx

--- src/app/pages/AdminAnalyticsPage.tsx
+++ src/app/pages/AdminAnalyticsPage.tsx
@@ -145,3 +145,4 @@ export function AdminAnalyticsPage() {
                 <linearGradient id="gradServed" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="#0ea5e9" stopOpacity={0.2} />
                   <stop offset="95%" stopColor="#0ea5e9" stopOpacity={0} />
+                </linearGradient>
