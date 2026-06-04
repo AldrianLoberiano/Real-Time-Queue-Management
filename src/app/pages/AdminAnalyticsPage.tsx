2c074b55490b328d721f3c57dbe267cf0c325d58
AldrianLoberiano <loberianorian@gmail.com>
2026-06-04 22:42:41 +0800

    Update AdminAnalyticsPage.tsx

--- src/app/pages/AdminAnalyticsPage.tsx
+++ src/app/pages/AdminAnalyticsPage.tsx
@@ -142,3 +142,4 @@ export function AdminAnalyticsPage() {
           <ResponsiveContainer width="100%" height={220}>
             <AreaChart data={hourlyData} margin={{ top: 5, right: 10, left: -20, bottom: 0 }}>
               <defs>
+                <linearGradient id="gradServed" x1="0" y1="0" x2="0" y2="1">
