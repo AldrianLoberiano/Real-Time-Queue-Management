edbe21cc0561120b79c24a31a8a70d472acdc21a
AldrianLoberiano <loberianorian@gmail.com>
2026-06-08 20:58:04 +0800

    Update AdminAnalyticsPage.tsx

--- src/app/pages/AdminAnalyticsPage.tsx
+++ src/app/pages/AdminAnalyticsPage.tsx
@@ -254,3 +254,4 @@ export function AdminAnalyticsPage() {
           ].map(item => (
             <div key={item.label} className="p-6 text-center">
               <div className={`w-10 h-10 rounded-xl mx-auto flex items-center justify-center mb-3 ${item.color}`}>
+                {item.icon}
