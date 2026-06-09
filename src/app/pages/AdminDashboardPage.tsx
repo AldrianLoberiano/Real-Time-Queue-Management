6ae4b19d767f15d811c5086529d03f8797a84d43
AldrianLoberiano <loberianorian@gmail.com>
2026-06-09 20:29:22 +0800

    Update AdminDashboardPage.tsx

--- src/app/pages/AdminDashboardPage.tsx
+++ src/app/pages/AdminDashboardPage.tsx
@@ -12,3 +12,4 @@ import { useQueue, type QueueItem, type PriorityType } from '../context/QueueCon
 
 function StatCard({ label, value, icon, color, sub }: {
   label: string; value: number | string; icon: React.ReactNode;
+  color: string; sub?: string;
