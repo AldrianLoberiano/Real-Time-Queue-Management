fe7d03171667bded8cf5a48c70b52a01e08f9aec
AldrianLoberiano <loberianorian@gmail.com>
2026-06-09 20:57:40 +0800

    Update AdminDashboardPage.tsx

--- src/app/pages/AdminDashboardPage.tsx
+++ src/app/pages/AdminDashboardPage.tsx
@@ -84,3 +84,4 @@ export function AdminDashboardPage() {
           <div className="bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden">
             <div className="px-5 py-4 border-b border-gray-100">
               <h2 className="font-semibold text-gray-700">Currently Serving</h2>
+            </div>
